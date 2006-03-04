2b74f918efcb5ff241fb78944d152b6497aff4f3
mkersten <mkersten>
2006-03-04 00:20:27 +0000

    Progress on: 129619: do UI revision and clean-up https://bugs.eclipse.org/bugs/show_bug.cgi?id=129619

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/views/TaskListTableLabelProvider.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/views/TaskListTableLabelProvider.java
@@ -146,10 +146,10 @@ public class TaskListTableLabelProvider extends DecoratingLabelProvider implemen
 						return TaskListImages.getImage(TaskListImages.STATUS_CONTEXT_CONFLICT);
 					}
 				}
-			}
-			} else {
-				return null;
-			}
+			} else if (element instanceof AbstractQueryHit){
+				return TaskListImages.getImage(TaskListImages.STATUS_NORMAL_INCOMING);
+			} 
+		}
 		return null;
 	}
 	
