72c2b845a85cb78b52ffcc88adae3a9fd6eddcff
mkersten <mkersten>
2006-04-01 01:54:35 +0000

    Progress on: 130203: make open corresponding task action generic, currently failing https://bugs.eclipse.org/bugs/show_bug.cgi?id=130203

--- org.eclipse.mylyn.ide.ui/src/org/eclipse/mylyn/internal/ide/ui/actions/OpenCorrespondingTaskAction.java
+++ org.eclipse.mylyn.ide.ui/src/org/eclipse/mylyn/internal/ide/ui/actions/OpenCorrespondingTaskAction.java
@@ -9,25 +9,23 @@
  *     University Of British Columbia - initial API and implementation
  *******************************************************************************/
 
-package org.eclipse.mylar.internal.bugs.ui.actions;
+package org.eclipse.mylar.internal.ide.ui.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.mylar.internal.bugs.java.OpenBugzillaReportJob;
-import org.eclipse.mylar.internal.bugzilla.core.BugzillaRepositoryUtil;
-import org.eclipse.mylar.internal.core.util.MylarStatusHandler;
 import org.eclipse.mylar.internal.ide.team.MylarContextChangeSet;
 import org.eclipse.mylar.internal.tasklist.ui.TaskListUiUtil;
+import org.eclipse.mylar.provisional.tasklist.AbstractRepositoryConnector;
+import org.eclipse.mylar.provisional.tasklist.MylarTaskListPlugin;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.team.internal.ccvs.core.client.listeners.LogEntry;
 import org.eclipse.team.internal.ui.synchronize.ChangeSetDiffNode;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.internal.ObjectPluginAction;
-import org.eclipse.ui.progress.IProgressService;
 
 /**
  * @author Mik Kersten
@@ -51,50 +49,26 @@ public class OpenCorrespondingTaskAction implements IViewActionDelegate {
 					comment = ((ChangeSetDiffNode) firstElement).getName();
 				} else if (firstElement instanceof LogEntry) {
 					comment = ((LogEntry) firstElement).getComment();
+				} else if (firstElement instanceof IFileRevision) {
+					comment = ((IFileRevision)firstElement).getComment();
 				}
 				if (comment != null) {
-					String idString = MylarContextChangeSet.getTaskIdFromComment(comment);
-					String url = MylarContextChangeSet.getUrlFromComment(comment);
-					String repositoryUrl = getRepositoryUrlFromComment(url);
-					int id = -1;
-					try {
-						id = Integer.parseInt(idString);
-					} catch (NumberFormatException e) {
-						// ignore
-					}
-					if (id != -1) {
-						// TODO: should try to open task first
-						OpenBugzillaReportJob job = new OpenBugzillaReportJob(repositoryUrl, id);
-						IProgressService service = PlatformUI.getWorkbench().getProgressService();
-						try {
-							service.run(true, false, job);
-						} catch (Exception e) {
-							MylarStatusHandler.fail(e, "Could not open report", true);
-						}
+					String fullUrl = MylarContextChangeSet.getUrlFromComment(comment);
+					AbstractRepositoryConnector connector = MylarTaskListPlugin.getRepositoryManager().getRepositoryForTaskUrl(fullUrl);
+					if (connector != null) {
+						String repositoryUrl = connector.getRepositoryUrlFromTaskUrl(fullUrl);
+						String id = MylarContextChangeSet.getTaskIdFromComment(comment);					
+						resolved = TaskListUiUtil.openRepositoryTask(repositoryUrl, id, fullUrl);
+					} else {
+						TaskListUiUtil.openUrl("Browser", "Browser", fullUrl);
 						resolved = true;
-					} else if (url != null) {
-						TaskListUiUtil.openUrl("Web Browser", "Web Browser", url);
-						resolved = true;
-					}
+					} 
 				}
 
 				if (!resolved) {
 					MessageDialog.openInformation(Display.getCurrent().getActiveShell(), "Mylar Information",
 							"Could not resolve report corresponding to change set comment.");
 				}
-			}
-		}
-	}
-
-	private String getRepositoryUrlFromComment(String comment) {
-		if (comment == null) {
-			return null;
-		} else {
-			int index = comment.indexOf(BugzillaRepositoryUtil.POST_ARGS_SHOW_BUG);
-			if (index != -1) {
-				return comment.substring(0, index);
-			} else {
-				return null;
 			}
 		}
 	}
