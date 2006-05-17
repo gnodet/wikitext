2240121f8484a984e9dbfdfc6f16ab26cdc96f7c
mkersten <mkersten>
2006-05-17 05:14:22 +0000

    Completed: 138303: Open report don't appear on popup menu in sync view for changeset model https://bugs.eclipse.org/bugs/show_bug.cgi?id=138303

---
+++ sandbox/org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/misc/IntrospectObjectAction.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 University Of British Columbia and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     University Of British Columbia - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylar.internal.sandbox.misc;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.mylar.provisional.core.IMylarElement;
+import org.eclipse.mylar.provisional.core.IMylarStructureBridge;
+import org.eclipse.mylar.provisional.core.MylarPlugin;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+/**
+ * @author Mik Kersten
+ */
+public class IntrospectObjectAction implements IViewActionDelegate {
+
+	private ISelection currentSelection;
+
+	public void init(IViewPart view) {
+
+	}
+
+	public void run(IAction action) {
+		if (currentSelection instanceof StructuredSelection) {
+			StructuredSelection selection = (StructuredSelection) currentSelection;
+			Object object = selection.getFirstElement();
+
+			String text = "Object class: " + object.getClass() + "\n\n";
+
+			try {
+				IMylarStructureBridge bridge = MylarPlugin.getDefault().getStructureBridge(object);
+				IMylarElement node = MylarPlugin.getContextManager().getElement(bridge.getHandleIdentifier(object));
+				if (node != null) {
+					text += "Interest value: " + node.getInterest().getValue() + "\n";
+					text += node.getInterest().toString();
+				}
+			} catch (Throwable t) {
+				text += "<no structure bridge>";
+			}
+
+			MessageDialog.openInformation(null, "Mylar Sandbox", text);
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.currentSelection = selection;
+	}
+
+}
