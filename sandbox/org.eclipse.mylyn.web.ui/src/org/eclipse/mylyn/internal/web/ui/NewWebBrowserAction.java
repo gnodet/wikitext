b1e9de5785bfe1f736f06cead6b15122060fe4a8
mkersten <mkersten>
2006-06-06 19:37:21 +0000

    Completed: 114966: fix web docs support https://bugs.eclipse.org/bugs/show_bug.cgi?id=114966

--- sandbox/org.eclipse.mylyn.web.ui/src/org/eclipse/mylyn/internal/web/ui/NewWebBrowserAction.java
+++ sandbox/org.eclipse.mylyn.web.ui/src/org/eclipse/mylyn/internal/web/ui/NewWebBrowserAction.java
@@ -11,9 +11,6 @@
 
 package org.eclipse.mylar.internal.web.ui;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -21,7 +18,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.internal.browser.WorkbenchBrowserSupport;
 
 /**
@@ -30,9 +26,9 @@ import org.eclipse.ui.internal.browser.WorkbenchBrowserSupport;
 public class NewWebBrowserAction implements IWorkbenchWindowActionDelegate {
 
 	public void run(IAction action) {
-		String url = "http://google.com"; 
+		String url = ""; 
 		try {
-			IWebBrowser browser = null;
+//			IWebBrowser browser = null;
 			int flags = 0;
 			if (WorkbenchBrowserSupport.getInstance().isInternalWebBrowserAvailable()) {
 				flags = WorkbenchBrowserSupport.AS_EDITOR | WorkbenchBrowserSupport.LOCATION_BAR
@@ -42,16 +38,17 @@ public class NewWebBrowserAction implements IWorkbenchWindowActionDelegate {
 				flags = WorkbenchBrowserSupport.AS_EXTERNAL | WorkbenchBrowserSupport.LOCATION_BAR
 						| WorkbenchBrowserSupport.NAVIGATION_BAR;
 			}
-			browser = WorkbenchBrowserSupport.getInstance().createBrowser(flags, "org.eclipse.mylar.tasklist",
+			WorkbenchBrowserSupport.getInstance().createBrowser(flags, "org.eclipse.mylar.tasklist",
 					"Browser", "tasktooltip");
-			browser.openURL(new URL(url));
+//			browser.openURL(new URL(url));
 		} catch (PartInitException e) {
 			MessageDialog.openError(Display.getDefault().getActiveShell(), "URL not found", url
 					+ " could not be opened");
-		} catch (MalformedURLException e) {
-			MessageDialog.openError(Display.getDefault().getActiveShell(), "URL not found", url
-					+ " could not be opened");
-		}
+		} 
+//		catch (MalformedURLException e) {
+//			MessageDialog.openError(Display.getDefault().getActiveShell(), "URL not found", url
+//					+ " could not be opened");
+//		}
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
