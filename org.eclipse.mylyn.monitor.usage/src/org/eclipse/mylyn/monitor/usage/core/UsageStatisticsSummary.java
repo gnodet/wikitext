2e14ec86c5433837e0b66a7b8a9df229daa85f1d
mkersten <mkersten>
2006-12-01 18:15:54 +0000

    NEW - bug 121309: improve monitor upload facility https://bugs.eclipse.org/bugs/show_bug.cgi?id=121309

---
+++ org.eclipse.mylyn.monitor.usage/src/org/eclipse/mylyn/monitor/usage/core/UsageStatisticsSummary.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 University Of British Columbia and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     University Of British Columbia - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylar.monitor.usage.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Container for statistics
+ * 
+ * @author Mik Kersten
+ */
+public class UsageStatisticsSummary {
+
+	private List<InteractionEventSummary> singleSummaries = new ArrayList<InteractionEventSummary>();
+
+	private List<IUsageCollector> collectors = new ArrayList<IUsageCollector>();
+
+	public List<InteractionEventSummary> getSingleSummaries() {
+		return singleSummaries;
+	}
+
+	public void setSingleSummaries(List<InteractionEventSummary> singleSummaries) {
+		this.singleSummaries = singleSummaries;
+	}
+
+	public void add(int index, IUsageCollector collector) {
+		collectors.add(index, collector);
+	}
+
+	public void add(IUsageCollector collector) {
+		collectors.add(collector);
+	}
+
+	public List<IUsageCollector> getCollectors() {
+		return collectors;
+	}
+}
---
+++ org.eclipse.mylyn.monitor.usage/src/org/eclipse/mylyn/monitor/usage/core/collectors/PercentUsageComparator.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 University Of British Columbia and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     University Of British Columbia - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylar.monitor.usage.core.collectors;
+
+import java.util.Comparator;
+
+/**
+ * @author Mik Kersten and Leah Findlater
+ */
+class PercentUsageComparator implements Comparator<String> {
+	public int compare(String o1, String o2) {
+		int index1 = o1.indexOf('%');
+		int index2 = o2.indexOf('%');
+		if (index1 != -1 && index2 != -1) {
+			String s1 = o1.substring(0, index1 - 1);
+			String s2 = o2.substring(0, index2 - 1);
+			return (-1) * new Float(s1).compareTo(new Float(s2));
+		} else {
+			return 0;
+		}
+	}
+}
