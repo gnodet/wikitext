fbfbed8c800539874aa55d25533c8b6cae884ae8
mkersten <mkersten>
2006-12-01 18:15:58 +0000

    NEW - bug 121309: improve monitor upload facility https://bugs.eclipse.org/bugs/show_bug.cgi?id=121309

--- sandbox/org.eclipse.mylyn.monitor.reports/src/org/eclipse/mylyn/internal/monitor/reports/MylarReportsPlugin.java
+++ sandbox/org.eclipse.mylyn.monitor.reports/src/org/eclipse/mylyn/internal/monitor/reports/MylarReportsPlugin.java
@@ -11,8 +11,8 @@
 
 package org.eclipse.mylar.internal.monitor.reports;
 
-import org.eclipse.ui.plugin.*;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
