7d299c55c4ab8edb193f3afb8d1d1184bf004944
relves <relves>
2007-02-23 00:14:57 +0000

    NEW - bug 152490: support due dates https://bugs.eclipse.org/bugs/show_bug.cgi?id=152490

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TaskListImages.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TaskListImages.java
@@ -69,7 +69,7 @@ public class TaskListImages {
 	
 	public static final ImageDescriptor TASK_NOTES = createSize(create(T_TOOL, "task-notes.gif"), SIZE);
 
-	public static final ImageDescriptor TASK_REPOSITORY_HISOTRY = createSize(create(T_TOOL, "task-repository-history.gif"), SIZE);
+	public static final ImageDescriptor TASK_REPOSITORY_HISTORY = createSize(create(T_TOOL, "task-repository-history.gif"), SIZE);
 	
 	public static final ImageDescriptor TASK_REPOSITORY_NOTES = createSize(create(T_TOOL, "task-repository-notes.gif"), SIZE);
 
@@ -80,6 +80,8 @@ public class TaskListImages {
 	public static final ImageDescriptor OVERLAY_WEB = create(T_TOOL, "overlay-web.gif");
  
 	public static final ImageDescriptor OVERLAY_SYNCHRONIZING = create(T_EVIEW, "overlay-synchronizing.gif");
+
+	public static final ImageDescriptor OVERLAY_DUE = create(T_EVIEW, "overlay-due.gif");
 	
 	public static final ImageDescriptor TASK_WEB = createWithOverlay(TASK, OVERLAY_WEB, false, true);
 
