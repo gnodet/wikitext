5e1bea878e5067e9aef3fa2a4f05a8aea44bd8fb
mkersten <mkersten>
2007-06-16 01:59:42 +0000

    NEW - bug 192995: fix task editor leak of handles due to embedded web browser leak https://bugs.eclipse.org/bugs/show_bug.cgi?id=192995

---
+++ org.eclipse.mylyn.web.tasks/src/org/eclipse/mylyn/internal/web/tasks/BrowserEditorInput.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 Mylar committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.web.tasks;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.mylyn.internal.tasks.ui.TasksUiImages;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.ui.editors.RepositoryTaskEditorInput;
+
+/**
+ * @author Mik Kersten
+ */
+public class BrowserEditorInput extends RepositoryTaskEditorInput {
+
+	public BrowserEditorInput(TaskRepository repository, String handle, String taskUrl, String taskId) {
+		super(repository, handle, taskUrl, taskId);
+	}
+
+	public ImageDescriptor getImageDescriptor() {
+		return TasksUiImages.BROWSER_SMALL;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final BrowserEditorInput other = (BrowserEditorInput) obj;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+
+
+	
+}
---
+++ org.eclipse.mylyn.web.tasks/src/org/eclipse/mylyn/internal/web/tasks/WebTaskEditorFactory.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 Mylar committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.web.tasks;
+
+import org.eclipse.mylyn.tasks.core.AbstractTask;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.ui.TasksUiPlugin;
+import org.eclipse.mylyn.tasks.ui.editors.AbstractRepositoryTaskEditor;
+import org.eclipse.mylyn.tasks.ui.editors.ITaskEditorFactory;
+import org.eclipse.mylyn.tasks.ui.editors.RepositoryTaskEditorInput;
+import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
+import org.eclipse.mylyn.tasks.ui.editors.TaskEditorInput;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.part.EditorPart;
+
+/**
+ * @author Mik Kersten
+ */
+public class WebTaskEditorFactory implements ITaskEditorFactory {
+
+	private static final String TITLE = "Browser";
+
+	public IEditorPart createEditor(TaskEditor parentEditor, IEditorInput editorInput) {
+		AbstractRepositoryTaskEditor editor = null;
+		if (editorInput instanceof TaskEditorInput) {
+			TaskEditorInput taskInput = (TaskEditorInput) editorInput;
+			return createBrowser(parentEditor, taskInput.getTask().getUrl());
+		}
+		return editor;
+	}
+
+	public IEditorInput createEditorInput(AbstractTask task) {
+		if (task instanceof WebTask) {
+			final TaskRepository repository = TasksUiPlugin.getRepositoryManager().getRepository(
+					WebRepositoryConnector.REPOSITORY_TYPE, task.getRepositoryUrl());
+			return new RepositoryTaskEditorInput(repository, task.getHandleIdentifier(), task.getUrl(),
+					task.getTaskId());
+		} else {
+			return null;
+		}
+	}
+
+	private EditorPart createBrowser(TaskEditor parentEditor, String url) {
+		return new BrowserFormPage(parentEditor, TITLE);
+	}
+
+	public String getTitle() {
+		return TITLE;
+	}
+
+	public boolean canCreateEditorFor(AbstractTask task) {
+		;
+		return task instanceof WebTask;
+	}
+
+	public boolean canCreateEditorFor(IEditorInput input) {
+		return input instanceof BrowserEditorInput;
+	}
+
+	public boolean providesOutline() {
+		return false;
+	}
+
+}
