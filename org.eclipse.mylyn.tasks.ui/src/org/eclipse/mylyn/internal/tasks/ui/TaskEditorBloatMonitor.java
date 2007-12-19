8e9e8057e8a2168370322a1d478f4d897bde01b5
mkersten <mkersten>
2007-12-19 00:56:04 +0000

    NEW - bug 206568: Improve performance and resource usage of bug reports with ~100 comments https://bugs.eclipse.org/bugs/show_bug.cgi?id=206568

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TaskEditorBloatMonitor.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TaskEditorBloatMonitor.java
@@ -17,7 +17,7 @@ import org.eclipse.ui.PartInitException;
  */
 public class TaskEditorBloatMonitor extends AbstractEditorTracker {
 
-	private int MAX_EDITORS = 12; 
+	private int MAX_EDITORS = 4;
 
 	@Override
 	protected void editorBroughtToTop(IEditorPart part) {
