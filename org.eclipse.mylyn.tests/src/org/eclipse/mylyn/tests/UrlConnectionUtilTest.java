f928c345e5aa0c9071a73a2c352fe359c0bfa518
spingel <spingel>
2008-03-01 22:52:26 +0000

    NEW - bug 167362: consume org.apache.httpclient dependency from Orbit https://bugs.eclipse.org/bugs/show_bug.cgi?id=167362

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/UrlConnectionUtilTest.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/UrlConnectionUtilTest.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 
-package org.eclipse.mylyn.tasks.tests;
+package org.eclipse.mylyn.tests;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy.Type;
