97ec381e23252da811586da335b8485d34629593
spingel <spingel>
2008-03-27 22:30:36 +0000

    NEW - bug 223066: [patch] RepositoryQueryPropertySource in sandbox.dev should display query Url as well https://bugs.eclipse.org/bugs/show_bug.cgi?id=223066

--- sandbox/org.eclipse.mylyn.sandbox.dev/src/org/eclipse/mylyn/internal/sandbox/dev/properties/RepositoryQueryPropertySource.java
+++ sandbox/org.eclipse.mylyn.sandbox.dev/src/org/eclipse/mylyn/internal/sandbox/dev/properties/RepositoryQueryPropertySource.java
@@ -12,7 +12,7 @@ import org.eclipse.ui.views.properties.TextPropertyDescriptor;
  * Display various {@link AbstractRepositoryQuery} properties in the Eclipse Properties View.<br />
  * See <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=210639">Bug 210639</a> and <a
  * href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=208275">Bug 208275</a><br />
- * 
+ *
  * @author Maarten Meijer
  */
 public class RepositoryQueryPropertySource extends AbstractTaskContainerPropertySource implements IPropertySource {
@@ -23,7 +23,9 @@ public class RepositoryQueryPropertySource extends AbstractTaskContainerProperty
 
 	private static final String STATUS = "status";
 
-	private static final String URL = "url";
+	private static final String REPOSITORY = "repository";
+
+	private static final String QUERY = "query";
 
 	private static final String KIND = "kind";
 
@@ -37,13 +39,15 @@ public class RepositoryQueryPropertySource extends AbstractTaskContainerProperty
 		summary.setCategory(description);
 		TextPropertyDescriptor kind = new TextPropertyDescriptor(KIND, "Repository Connector Kind");
 		kind.setCategory(description);
-		TextPropertyDescriptor url = new TextPropertyDescriptor(URL, "Repository URL");
+		TextPropertyDescriptor url = new TextPropertyDescriptor(REPOSITORY, "Repository URL");
 		url.setCategory(description);
+		TextPropertyDescriptor query = new TextPropertyDescriptor(QUERY, "Query URL");
+		query.setCategory(description);
 		TextPropertyDescriptor status = new TextPropertyDescriptor(STATUS, "Synchronization Status");
 		status.setCategory(description);
 		TextPropertyDescriptor lastRead = new TextPropertyDescriptor(LAST_READ, "Synchronization Timestamp");
 		lastRead.setCategory(description);
-		IPropertyDescriptor[] specific = new IPropertyDescriptor[] { summary, kind, url, status, lastRead, };
+		IPropertyDescriptor[] specific = new IPropertyDescriptor[] { summary, kind, url, query, status, lastRead, };
 		return super.appendSpecifics(specific, super.getPropertyDescriptors());
 	}
 
@@ -54,8 +58,10 @@ public class RepositoryQueryPropertySource extends AbstractTaskContainerProperty
 			return query.getSummary();
 		} else if (KIND.equals(id)) {
 			return query.getRepositoryKind();
-		} else if (URL.equals(id)) {
+		} else if (REPOSITORY.equals(id)) {
 			return query.getRepositoryUrl();
+		} else if (QUERY.equals(id)) {
+			return query.getUrl();
 		} else if (STATUS.equals(id)) {
 			return query.getSynchronizationStatus() == null ? NULL_MSG : query.getSynchronizationStatus().toString();
 		} else if (LAST_READ.equals(id)) {
