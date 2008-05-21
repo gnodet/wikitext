79c9b7e4453d3f540bc414cab2e77e017e29d1c3
spingel <spingel>
2008-05-21 04:07:33 +0000

    NEW - bug 150278: [api] associate plugins with task repositories and products https://bugs.eclipse.org/bugs/show_bug.cgi?id=150278

--- sandbox/org.eclipse.mylyn.xplanner.tests/src/org/eclipse/mylyn/xplanner/tests/XPlannerClientFacadeTest.java
+++ sandbox/org.eclipse.mylyn.xplanner.tests/src/org/eclipse/mylyn/xplanner/tests/XPlannerClientFacadeTest.java
@@ -7,52 +7,28 @@
  *******************************************************************************/
 package org.eclipse.mylyn.xplanner.tests;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.mylyn.commons.core.IStatusHandler;
-import org.eclipse.mylyn.commons.core.StatusHandler;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.xplanner.ui.XPlannerClientFacade;
 
+/**
+ * @author Helen Bershadskaya
+ */
 public class XPlannerClientFacadeTest extends TestCase {
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
 
 	public void testGetXPlannerClientWithBadConnectionEnsureNoStatus() throws Exception {
 		TaskRepository repository = XPlannerTestUtils.getRepository();
 		String goodUrl = repository.getRepositoryUrl();
-		repository.setRepositoryUrl(XPlannerTestUtils.BAD_REPOSITORY_LOCATION);
-
-		IStatusHandler fakeHandler = new IStatusHandler() {
-
-			public void displayStatus(String title, IStatus status) {
-				Assert.fail("testGetXPlannerClientWithBadConnectionEnsureNoStatus() -- status handler called");
-			}
-
-			public void fail(IStatus status, boolean informUser) {
-				Assert.fail("testGetXPlannerClientWithBadConnectionEnsureNoStatus() -- status handler called");
-			}
-
-		};
-
-		StatusHandler.addStatusHandler(fakeHandler);
 		try {
+			repository.setRepositoryUrl(XPlannerTestUtils.BAD_REPOSITORY_LOCATION);
 			// ensure no status message gets displayed during this call
 			XPlannerClientFacade.getDefault().getXPlannerClient(repository);
+			fail("Expected CoreException");
 		} catch (CoreException e) {
-			assertTrue(e.getMessage() != null && e.getMessage().contains("Error connecting"));
+			assertEquals("Error connecting", e.getMessage());
 		} finally {
-			StatusHandler.removeStatusHandler(fakeHandler);
 			repository.setRepositoryUrl(goodUrl);
 		}
 	}
