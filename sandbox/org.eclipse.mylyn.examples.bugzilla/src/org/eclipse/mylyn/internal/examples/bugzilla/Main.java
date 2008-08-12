9f0f1882154314bb39fea3dcd78460c881bb8527
spingel <spingel>
2008-08-12 01:46:45 +0000

    NEW - bug 242983: bugzilla connector does not work in standalone environments https://bugs.eclipse.org/bugs/show_bug.cgi?id=242983

--- sandbox/org.eclipse.mylyn.examples.bugzilla/src/org/eclipse/mylyn/internal/examples/bugzilla/Main.java
+++ sandbox/org.eclipse.mylyn.examples.bugzilla/src/org/eclipse/mylyn/internal/examples/bugzilla/Main.java
@@ -23,10 +23,6 @@ import org.eclipse.mylyn.tasks.core.data.TaskData;
  */
 public class Main {
 
-	private static final String USERNAME = "username";
-
-	private static final String PASSWORD = "password";
-
 	private static final String URL = "https://landfill.bugzilla.org/bugzilla-tip/";
 
 	public static void main(String[] args) {
@@ -34,8 +30,10 @@ public class Main {
 		TaskRepository repository = new TaskRepository(BugzillaCorePlugin.CONNECTOR_KIND, URL);
 
 		// set repository credentials
-		AuthenticationCredentials credentials = new AuthenticationCredentials(USERNAME, PASSWORD);
-		repository.setCredentials(AuthenticationType.REPOSITORY, credentials, false);
+		if (args.length >= 2) {
+			AuthenticationCredentials credentials = new AuthenticationCredentials(args[0], args[1]);
+			repository.setCredentials(AuthenticationType.REPOSITORY, credentials, false);
+		}
 
 		// create bugzilla connector
 		BugzillaRepositoryConnector connector = new BugzillaRepositoryConnector();
@@ -46,18 +44,19 @@ public class Main {
 
 			// access task information
 			ITaskMapping taskMapping = connector.getTaskMapping(taskData);
-			System.out.println(taskMapping.getSummary());
-			System.out.println(taskMapping.getPriority());
+			System.out.println("Summary:     " + taskMapping.getSummary());
+			System.out.println("Priority:    " + taskMapping.getPriority());
 
 			// access report data via attributes
 			TaskAttribute descriptionAttribute = taskData.getRoot().getMappedAttribute(TaskAttribute.DESCRIPTION);
-			System.out.println(descriptionAttribute.getValue());
+			System.out.println("Description: " + descriptionAttribute.getValue());
 
 			// ...or by Bugzilla keys
 			TaskAttribute severityAttribute = taskData.getRoot().getAttribute(BugzillaAttribute.BUG_SEVERITY.getKey());
-			System.out.println(severityAttribute.getValue());
+			System.out.println("Severity:    " + severityAttribute.getValue());
 
 			// Post modified report to repository
+			System.out.println("\nPosting a new description...");
 			descriptionAttribute.setValue("Hello world.");
 			connector.getTaskDataHandler().postTaskData(repository, taskData, null, null);
 		} catch (CoreException e) {
