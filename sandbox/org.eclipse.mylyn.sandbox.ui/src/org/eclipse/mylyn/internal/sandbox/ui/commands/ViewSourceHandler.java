555b62aa4f2b55fd0ca8d3c4c400507365b0f58d
mkersten <mkersten>
2008-09-13 03:38:54 +0000

    NEW - bug 194157: update copyright notices to 2008 https://bugs.eclipse.org/bugs/show_bug.cgi?id=194157

--- sandbox/org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/ui/commands/ViewSourceHandler.java
+++ sandbox/org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/ui/commands/ViewSourceHandler.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Jingwen Ou and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Kingwen Ou - initial API and implementation
+ *     Jingwen Ou - initial API and implementation
  *     Tasktop Technologies - improvements
  *******************************************************************************/
 
