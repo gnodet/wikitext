404498dca21cf6681ce50e5fe562b4738520f98f
spingel <spingel>
2008-10-17 03:11:33 +0000

    NEW - bug 237596: [sandbox] deadlock in SandboxUiPlugin.initializeHighlighters() https://bugs.eclipse.org/bugs/show_bug.cgi?id=237596

--- sandbox/org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/ui/SandboxUiPlugin.java
+++ sandbox/org.eclipse.mylyn.sandbox.ui/src/org/eclipse/mylyn/internal/sandbox/ui/SandboxUiPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -172,6 +172,11 @@ public class SandboxUiPlugin extends AbstractUIPlugin {
 	 * @return null if not found
 	 */
 	public Highlighter getHighlighter(String name) {
+		// TODO 3.1 reenable when bug 237596 is fixed
+		if (true) {
+			return null;
+		}
+
 		if (highlighters == null) {
 			initializeHighlighters();
 		}
