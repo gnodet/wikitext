0b01333fab6645217fe5b43946101b5dc0acbdc1
relves <relves>
2009-01-29 16:16:34 +0000

    NEW - bug 260963: [patch] string compare for Bugzilla versions is not robust https://bugs.eclipse.org/bugs/show_bug.cgi?id=260963

--- org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/BugzillaVersion.java
+++ org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/BugzillaVersion.java
@@ -1,39 +1,30 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 Eugene Kuleshov and others.
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Eugene Kuleshov - initial API and implementation
+ *     Tasktop Technologies - initial API and implementation
  *******************************************************************************/
 
-package org.eclipse.mylyn.internal.jira.core.model;
+package org.eclipse.mylyn.internal.bugzilla.core;
 
 /**
- * JIRA version holder
- * 
- * @author Eugene Kuleshov
- * @author Thomas Ehrnhoefer
+ * @author Frank Becker
  */
-public class JiraVersion implements Comparable<JiraVersion> {
+public class BugzillaVersion implements Comparable<BugzillaVersion> {
 
-	public final static JiraVersion MIN_VERSION = new JiraVersion("3.3.3"); //$NON-NLS-1$
+	public final static BugzillaVersion MIN_VERSION = new BugzillaVersion("2.18"); //$NON-NLS-1$
 
-	public static final JiraVersion JIRA_3_3 = new JiraVersion("3.3"); //$NON-NLS-1$
+	public final static BugzillaVersion BUGZILLA_2_18 = new BugzillaVersion("2.18"); //$NON-NLS-1$
 
-	public static final JiraVersion JIRA_3_4 = new JiraVersion("3.4"); //$NON-NLS-1$
+	public final static BugzillaVersion BUGZILLA_3_0 = new BugzillaVersion("3.0"); //$NON-NLS-1$
 
-	public static final JiraVersion JIRA_3_7 = new JiraVersion("3.7"); //$NON-NLS-1$
+	public final static BugzillaVersion BUGZILLA_3_2 = new BugzillaVersion("3.2"); //$NON-NLS-1$
 
-	public static final JiraVersion JIRA_3_10 = new JiraVersion("3.10"); //$NON-NLS-1$
-
-	public static final JiraVersion JIRA_3_11 = new JiraVersion("3.11"); //$NON-NLS-1$
-
-	public static final JiraVersion JIRA_3_12 = new JiraVersion("3.12"); //$NON-NLS-1$
-
-	public static final JiraVersion JIRA_3_13 = new JiraVersion("3.13"); //$NON-NLS-1$
+	public final static BugzillaVersion MAX_VERSION = new BugzillaVersion("3.2"); //$NON-NLS-1$
 
 	private final int major;
 
@@ -41,14 +32,11 @@ public class JiraVersion implements Comparable<JiraVersion> {
 
 	private final int micro;
 
-	private final String qualifier;
-
-	public JiraVersion(String version) {
+	public BugzillaVersion(String version) {
 		String[] segments = version == null ? new String[0] : version.split("\\."); //$NON-NLS-1$
 		major = segments.length > 0 ? parse(segments[0]) : 0;
 		minor = segments.length > 1 ? parse(segments[1]) : 0;
 		micro = segments.length > 2 ? parse(segments[2]) : 0;
-		qualifier = segments.length == 0 ? "" : getQualifier(segments[segments.length - 1]); //$NON-NLS-1$
 	}
 
 	private int parse(String segment) throws NumberFormatException {
@@ -60,19 +48,11 @@ public class JiraVersion implements Comparable<JiraVersion> {
 		return n == -1 ? segment : segment.substring(0, n);
 	}
 
-	private String getQualifier(String segment) {
-		int n = segment.indexOf('-');
-		return n == -1 ? "" : segment.substring(n + 1); //$NON-NLS-1$
-	}
-
-	public boolean isSmallerOrEquals(JiraVersion v) {
+	public boolean isSmallerOrEquals(BugzillaVersion v) {
 		return compareTo(v) <= 0;
 	}
 
-	/**
-	 * 3.6.5-#161 3.9-#233 3.10-DEV-190607-#251
-	 */
-	public int compareTo(JiraVersion v) {
+	public int compareTo(BugzillaVersion v) {
 		if (major < v.major) {
 			return -1;
 		} else if (major > v.major) {
@@ -91,8 +71,22 @@ public class JiraVersion implements Comparable<JiraVersion> {
 			return 1;
 		}
 
-		// qualifier is not needed to compare with min version
-		return qualifier.compareTo(v.qualifier);
+		return 0;
+	}
+
+	public int compareMajorMinorOnly(BugzillaVersion v) {
+		if (major < v.major) {
+			return -1;
+		} else if (major > v.major) {
+			return 1;
+		}
+
+		if (minor < v.minor) {
+			return -1;
+		} else if (minor > v.minor) {
+			return 1;
+		}
+		return 0;
 	}
 
 	@Override
@@ -102,9 +96,6 @@ public class JiraVersion implements Comparable<JiraVersion> {
 		sb.append(".").append(Integer.toString(minor)); //$NON-NLS-1$
 		if (micro > 0) {
 			sb.append(".").append(Integer.toString(micro)); //$NON-NLS-1$
-		}
-		if (qualifier.length() > 0) {
-			sb.append("-").append(qualifier); //$NON-NLS-1$
 		}
 		return sb.toString();
 	}
