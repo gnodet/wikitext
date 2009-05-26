31f9b74719a070483a133f54928897754261760f
spingel <spingel>
2009-05-26 03:37:27 +0000

    ASSIGNED - bug 243655: [patch] notification window shows last unread comment, not the newest one https://bugs.eclipse.org/bugs/show_bug.cgi?id=243655

--- sandbox/org.eclipse.mylyn.sandbox.dev/src/org/eclipse/mylyn/internal/sandbox/dev/actions/TestTaskListNotificationAction.java
+++ sandbox/org.eclipse.mylyn.sandbox.dev/src/org/eclipse/mylyn/internal/sandbox/dev/actions/TestTaskListNotificationAction.java
@@ -12,14 +12,15 @@
 package org.eclipse.mylyn.internal.sandbox.dev.actions;
 
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Date;
 import java.util.Iterator;
-import java.util.Set;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.mylyn.internal.tasks.core.AbstractTask;
+import org.eclipse.mylyn.internal.tasks.core.LocalTask;
 import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
+import org.eclipse.mylyn.internal.tasks.ui.notifications.TaskListNotification;
 import org.eclipse.mylyn.internal.tasks.ui.notifications.TaskListNotificationReminder;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
@@ -42,15 +43,27 @@ public class TestTaskListNotificationAction implements IWorkbenchWindowActionDel
 	public void run(IAction action) {
 		Collection<AbstractTask> allTasks = TasksUiPlugin.getTaskList().getAllTasks();
 		Iterator<AbstractTask> iterator = allTasks.iterator();
-		Set<TaskListNotificationReminder> dummyNotifications = new HashSet<TaskListNotificationReminder>();
-		for (int i = 0; i < 6; i++) {
-			TaskListNotificationReminder notification = new TaskListNotificationReminder(iterator.next());
-			notification.setDescription("Mylyn is the Task-Focused UI for Eclipse that reduces information overload "
-					+ "\n..........and makes multi-tasking easy. It does this by making tasks a first class part of");
-//					+ "Eclipse, and integrating rich and offline editing for repositories such as Bugzilla, "
-//					+ "Trac, and JIRA. Once your tasks are integrated, Mylyn monitors your work activity to "
-//					+ "inidentify information relevant to the task-at-hand, and uses this task context to focus ");
-			dummyNotifications.add(notification);
+
+		TaskListNotification notification = new TaskListNotificationReminder(new LocalTask("1",
+				"This notification has a long description"));
+		notification.setDescription("Mylyn is the Task-Focused UI for Eclipse that reduces information overload "
+				+ "\n..........and makes multi-tasking easy. It does this by making tasks a first class part of"
+				+ "Eclipse, and integrating rich and offline editing for repositories such as Bugzilla, "
+				+ "Trac, and JIRA. Once your tasks are integrated, Mylyn monitors your work activity to "
+				+ "identify information relevant to the task-at-hand, and uses this task context to focus ");
+		TasksUiPlugin.getTaskListNotificationManager().getNotifications().add(notification);
+		notification.setDate(new Date());
+
+		notification = new TaskListNotificationReminder(new LocalTask("1", "Very old task"));
+		notification.setDate(new Date(0));
+		TasksUiPlugin.getTaskListNotificationManager().getNotifications().add(notification);
+
+		notification = new TaskListNotificationReminder(new LocalTask("1", "Very recent task"));
+		notification.setDate(new Date());
+		TasksUiPlugin.getTaskListNotificationManager().getNotifications().add(notification);
+
+		for (int i = 0; i < 6 && i < allTasks.size(); i++) {
+			notification = new TaskListNotification(iterator.next());
 			TasksUiPlugin.getTaskListNotificationManager().getNotifications().add(notification);
 		}
 		TasksUiPlugin.getTaskListNotificationManager().showPopup();
