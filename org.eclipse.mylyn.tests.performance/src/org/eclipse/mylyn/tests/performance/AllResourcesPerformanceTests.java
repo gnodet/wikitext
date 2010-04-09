adfafbbd0d6ac9beebe01eafdb8dc9cd17faf9bc
sminto <sminto>
2010-04-09 18:07:36 +0000

    ASSIGNED - bug 298688: [context] support ant filters for resource monitoring exclusions https://bugs.eclipse.org/bugs/show_bug.cgi?id=298688

--- org.eclipse.mylyn.tests.performance/src/org/eclipse/mylyn/tests/performance/AllResourcesPerformanceTests.java
+++ org.eclipse.mylyn.tests.performance/src/org/eclipse/mylyn/tests/performance/AllResourcesPerformanceTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 Tasktop Technologies and others.
+ * Copyright (c) 2010 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,16 +14,15 @@ package org.eclipse.mylyn.tests.performance;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-/**
- * @author Steffen Pingel
- */
-public class AllPerformanceTests {
+import org.eclipse.mylyn.resources.tests.performance.ResourceChangeMonitorPerformanceTest;
 
+/**
+ * @author Shawn Minto
+ */
+public class AllResourcesPerformanceTests {
 	public static Test suite() {
-		TestSuite suite = new TestSuite("Performance tests for org.eclipse.mylyn.tests.performance");
-		suite.addTest(AllTasksPerformanceTests.suite());
-		suite.addTest(AllResourcesPerformanceTests.suite());
+		TestSuite suite = new TestSuite("Test for org.eclipse.mylyn.resources.tests.performance");
+		suite.addTestSuite(ResourceChangeMonitorPerformanceTest.class);
 		return suite;
 	}
-
 }
